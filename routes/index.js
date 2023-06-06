dd7d7fbb2167713b5b61e1b7b578dac1a34aa248
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2023-06-06 03:16:23 +0300

    feat: movies routes

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const auth = require('../middlewares/auth');
 const signupRoute = require('./signup');
 const signinRoute = require('./signin');
 const usersRoutes = require('./users');
+const moviesRoutes = require('./movies');
 
 router.use('/', signupRoute);
 router.use('/', signinRoute);
 
 router.use('/users', auth, usersRoutes);
+router.use('/movies', auth, moviesRoutes);
 
 // router.use((req, res, next) => next(new PageNotFound('Страница не найдена')));
 
